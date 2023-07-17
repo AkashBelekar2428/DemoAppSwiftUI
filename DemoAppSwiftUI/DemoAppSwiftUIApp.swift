f81ea3f18d8ffa8aa7014a4b15d2f4a2a96f3461
Akash Belekar <akash.belekar@mobifilia.com>
2023-07-17 10:00:56 +0530

    Regiter Form using swift UI

---
+++ DemoAppSwiftUI/DemoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppSwiftUIApp.swift
+//  DemoAppSwiftUI
+//
+//  Created by Akash Belekar on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
